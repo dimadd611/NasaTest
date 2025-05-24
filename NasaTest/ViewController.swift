bd98fb6f6d4c4c7d522e7e62bd2515eb260f23f8
Rahmaniuc Dmitrii <dima.twari@Mac-mini-Dmitrij.local>
2025-05-24 12:32:36 +0300

    First commit

---
+++ NasaTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NasaTest
+//
+//  Created by Дмитрий Рахманюк on 24.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
